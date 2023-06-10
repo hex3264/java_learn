e9477a41c28bf381a58e0ebf005329f7c3a2b213
hex3264 <hex6@yandex.ru>
2023-06-10 18:39:55 +0300

    1
    Добавлен расчет между точками

--- sandbox/src/main/java/ru/stqa/java_learn/sandbox/firstprogram.java
+++ sandbox/src/main/java/ru/stqa/java_learn/sandbox/firstprogram.java
@@ -3,7 +3,9 @@ package ru.stqa.java_learn.sandbox;
 public class FirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
+        Point P = new Point(3, 2, 1, 4);
+        System.out.println("Расстояние между точками  = " + P.distance());
     }
 
 }
