6b6bb845967bf53fd089620d91e11c5eeabb7fa6
hex3264 <hex6@yandex.ru>
2023-06-25 19:41:19 +0300

    1
    Создан общий базовый класс

--- addressbook-web-tests/src/test/java/ru/stqa/java_learn/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java_learn/addressbook/GroupCreationTests.java
@@ -1,35 +1,8 @@
 package ru.stqa.java_learn.addressbook;
 
-import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GroupCreationTests {
-    private WebDriver wd;
-    private JavascriptExecutor js;
-
-    @BeforeMethod(alwaysRun = true)
-    public void setUp() throws Exception {
-        wd = new ChromeDriver();
-        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        js = (JavascriptExecutor) wd;
-        wd.get("http://localhost/addressbook/group.php");
-        login();
-    }
-
-    private void login() {
-        login("admin", "secret");
-    }
-
-    private void login(String username, String password) {
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//input[@value='Login']")).click();
-    }
+public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() throws Exception {
@@ -39,46 +12,4 @@ public class GroupCreationTests {
         returnToGroupPage();
     }
 
-    private void returnToGroupPage() {
-        wd.findElement(By.linkText("group page")).click();
-    }
-
-    private void submitGroupCreation() {
-        wd.findElement(By.name("submit")).click();
-    }
-
-    private void fillGroupForm(GroupDate groupDate) {
-        wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys(groupDate.getName());
-        wd.findElement(By.name("group_header")).click();
-        wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys(groupDate.getHeader());
-        wd.findElement(By.name("group_footer")).click();
-        wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys(groupDate.getFooter());
-    }
-
-    private void goToGroupPage() {
-        wd.findElement(By.name("new")).click();
-        wd.findElement(By.name("group_name")).click();
-    }
-
-    @AfterMethod(alwaysRun = true)
-    public void tearDown() throws Exception {
-        logout();
-        wd.quit();
-    }
-
-    private void logout() {
-        wd.findElement(By.linkText("Logout")).click();
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/java_learn/addressbook/GroupDeletionTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.java_learn.addressbook;
+
+import org.testng.annotations.*;
+
+public class GroupDeletionTests extends TestBase {
+
+    @Test
+    public void testGroupDeletion() {
+        goToGroupPage();
+        selectGroup();
+        deleteSelectedGroups();
+        returnToGroupPage();
+    }
+
+}
+
+
+
