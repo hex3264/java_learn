53d6419e2d94e002d1599fced4707d0e8288bdba
hex3264 <hex6@yandex.ru>
2023-07-09 19:21:26 +0300

    Проверка предусловий

--- addressbook-web-tests/src/test/java/ru/stqa/java_learn/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java_learn/addressbook/tests/GroupDeletionTests.java
@@ -1,15 +1,19 @@
 package ru.stqa.java_learn.addressbook.tests;
 
 import org.testng.annotations.*;
+import ru.stqa.java_learn.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
     @Test
     public void testGroupDeletion() {
         app.getNavigationHelper().goToGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
-        app.getNavigationHelper().gotoHomePage();
+        app.getGroupHelper().returnToGroupPage();
     }
 
 }
